3f43b60d1d66ad3321f1526dcc11543a101ee09a
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-07-26 01:36:56 +0600

    Deployed website

--- src/components/ProjectSidebar.jsx
+++ src/components/ProjectSidebar.jsx
@@ -4,7 +4,7 @@ export default function ProjectSidebar({
   onStartAddProject,
   projects,
   onSelectProject,
-  selectedProjectId
+  selectedProjectId,
 }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl h-screen">
@@ -16,22 +16,28 @@ export default function ProjectSidebar({
         <ButtonAdd onClick={onStartAddProject}>+ Add Project</ButtonAdd>
       </div>
       <ul className="mt-8">
-        {projects.map((project) =>{
-          let cssClasses = "w-full text-left px-2 py-1 mb-1 rounded-sm hover:text-stone-200 hover:bg-stone-800";
-          
-          if(project.id === selectedProjectId) {
-            cssClasses +=' bg-stone-800 text-stone-200'
-          } else{
-            cssClasses +=' text-stone-400';
+        {projects.map((project) => {
+          let cssClasses =
+            "w-full text-left px-2 py-1 mb-1 rounded-sm hover:text-stone-200 hover:bg-stone-800";
+
+          if (project.id === selectedProjectId) {
+            cssClasses += " bg-stone-800 text-stone-200";
+          } else {
+            cssClasses += " text-stone-400";
           }
-                    
-          return (<li key={project.id}>
-            {" "}
-            <button className={cssClasses} onClick={()=>onSelectProject(project.id)}>
-              {project.title}            
-            </button>
-          </li>);}
-        )}
+
+          return (
+            <li key={project.id}>
+              {" "}
+              <button
+                className={cssClasses}
+                onClick={() => onSelectProject(project.id)}
+              >
+                {project.title}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
